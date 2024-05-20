d95c2a9c46d91e9cc9385ec36ab6173625cb22a8
fernicolonthree <f2459902@gmail.com>
2024-05-19 21:33:50 -0400

    Update empleados.cpp

--- empleados.cpp
+++ empleados.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main() {
-    int ne; // Declaramos la variable para almacenar el número de empleados
+    int ne; // Declaramos la variable para almacenar el número de empleado
 
-    cout << "Introduzca el número de empleados: "; // Pedimos al usuario que introduzca el número de empleados
+    cout << "Introduzca el número de empleados: "; // Pedimos al usuario que introduzca el número de empleado
     cin >> ne; // Leemos el número introducido por el usuario y lo almacenamos en la variable "ne"
 
-    // Verificamos si el número de empleados está fuera del rango válido (1000-3000)
+    // Verificamos si el número de empleado está fuera del rango válido (1000-3000)
     if (ne < 1000 || ne > 3000) {  
-        cout << "Compañía no válida. Complete este programa." << endl; // Si está fuera del rango, mostramos un mensaje de error
+        cout << "Empleado valido." << endl; // Si está fuera del rango, mostramos un mensaje de error
     } else {
-        cout << "Compañía válida" << endl; // Si está dentro del rango, mostramos un mensaje de éxito
+        cout << "Empleado no valido" << endl; // Si está dentro del rango, mostramos un mensaje de éxito
     }
 
     return 0; // Finalizamos el programa
